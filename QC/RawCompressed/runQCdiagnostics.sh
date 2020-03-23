49d27f2c3ac1031af5184b6cc5cd81f49d705d9e
Nicolo' Jacazio <nicolo.jacazio@cern.ch>
2020-03-23 11:51:50 +0100

    Rename json

--- QC/RawCompressed/runQCdiagnostics.sh
+++ QC/RawCompressed/runQCdiagnostics.sh
@@ -6,7 +6,7 @@ o2-raw-file-reader-workflow -b --message-per-tf --conf o2-raw-reader-workflow.in
        	| \
        	o2-tof-compressor -b \
 	| \
-	o2-qc -b --config json://${PWD}/tofcompressed.json
+	o2-qc -b --config json://${PWD}/tofdiagnostics.json
 
 rm localhost*
 
