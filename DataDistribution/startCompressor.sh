21079b4fcf21966b5064b7870adfe948ac41b50f
Roberto Preghenella <preghenella@bo.infn.it>
2020-04-27 19:05:39 +0200

    Minor updates to DataDistribution test suite

--- DataDistribution/startCompressor.sh
+++ DataDistribution/startCompressor.sh
@@ -3,7 +3,7 @@
 o2-dpl-raw-proxy -b --session default \
     --dataspec "A:TOF/RAWDATA" \
     --channel-config "name=readout-proxy,type=pull,method=connect,address=ipc:///tmp/stf-builder-dpl-pipe-0,transport=shmem,rateLogging=1" \
-    | o2-tof-compressor -b \
+    | o2-tof-compressor -b --tof-compressor-rdh-version 4 \
     | o2-dpl-output-proxy -b --session default \
     --dataspec "downstream:TOF/CRAWDATA" \
     --channel-config "name=downstream,type=push,method=bind,address=ipc:///tmp/stf-pipe-0,rateLogging=1,transport=shmem"
